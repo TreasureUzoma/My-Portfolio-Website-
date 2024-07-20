9685abb96e2b5676ed1702feedf64d87c04bb3fb
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2024-07-20 09:02:09 +0100

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,1 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const block1 = document.querySelectorAll('.block-1');
+    const block2 = document.querySelectorAll('.block-2');
 
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__pulse');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+    const observer2 = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__zoomIn');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    block1.forEach(block => {
+        observer.observe(block);
+    });
+    block2.forEach(block => {
+        observer2.observe(block);
+    });
+});
